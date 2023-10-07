0ae35889dfcf28288b37cc407c0d45d4c7b19bb2
mgrkan <fogrkan@gmail.com>
2023-10-08 01:42:26 +0300

    now reads all the words beforehand

--- main.py
+++ main.py
@@ -17,17 +17,38 @@ driver.get("https://10fastfingers.com/typing-test/turkish")
 
 deny = driver.find_element(By.ID, "CybotCookiebotDialogBodyButtonDecline")
 deny.click()
+
 #words = driver.find_elements(By.ID, "row1")
 #print(len(words))
 #words = words[0].text
 #print(words)
 #words = words.split(" ")
 #print(words)
+
+#for i in range(1, 500):
+#    word = driver.find_element(By.CLASS_NAME, "highlight")
+#    word = word.text
+#    text_input.send_keys(word)
+#    text_input.send_keys(Keys.SPACE)
+words = []
+i = -1
+while True:
+    i += 1
+    el = "//span[@wordnr='{}']".format(i)
+    print(el)
+    try:
+        word = driver.find_element(By.XPATH, el)
+        driver.execute_script("arguments[0].scrollIntoView();", word)
+        print(word, word.text)
+        words.append(word.text)
+    except:
+        word = driver.find_element(By.XPATH, "//span[@wordnr='1']")
+        driver.execute_script("arguments[0].scrollIntoView();", word)
+        break
 text_input = driver.find_element(By.ID, "inputfield")
-for i in range(1, 500):
-    word = driver.find_element(By.CLASS_NAME, "highlight")
-    word = word.text
-    text_input.send_keys(word)
+for i in words:
+    text_input.send_keys(i)
     text_input.send_keys(Keys.SPACE)
+
 time.sleep(20)
 
